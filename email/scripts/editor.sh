7c5647f72e86d63feae8312a13f9b271f8fd780c
Tarak Shah <tarak@hrdag.org>
2019-09-18 09:35:07 -0700

    email updates

---
+++ email/scripts/editor.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# based on https://github.com/wincent/wincent/blob/master/roles/dotfiles/files/.mutt/scripts/view_mail.sh
+
+COLUMNS=${COLUMNS:-`tput cols`}
+
+if [ -z "$TMUX" ]; then
+  nvim '+set filetype=mail' '+set syntax=markdown' '+set nofoldenable' '+Goyo' "$1"
+elif [ "$COLUMNS" -gt 180 ]; then
+    tmux split-window -p 50 -h \
+        neomutt -F '~/.mutt/editor.muttrc' '-H' "$1"
+else
+    tmux split-window -p 50 -v \
+        neomutt -F '~/.mutt/editor.muttrc' '-H' "$1"
+fi
+
+rm "$1"
+
+# done.
---
+++ email/scripts/view_mail.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# based on https://github.com/wincent/wincent/blob/master/roles/dotfiles/files/.mutt/scripts/view_mail.sh
+
+COLUMNS=${COLUMNS:-`tput cols`}
+TMPFILE="$1"
+#ENCODING="${2:utf-8}"
+
+# Need to copy the file because mutt will delete it before Vim can read it.
+DIR=$(mktemp -d)
+cp "$TMPFILE" "$DIR/preview"
+TMPFILE="$DIR/preview"
+
+if [ -z "$TMUX" ]; then
+  nvim -R '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' '+Goyo' "$TMPFILE"
+elif [ "$COLUMNS" -gt 180 ]; then
+  tmux split-window -p 50 -h \
+    nvim -R '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' '+Goyo' "$TMPFILE"
+else
+  tmux split-window -p 50 -v \
+    nvim -R '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' '+Goyo' "$TMPFILE"
+fi
